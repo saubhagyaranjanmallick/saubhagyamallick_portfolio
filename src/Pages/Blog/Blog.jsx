44fe649c85fb15a6d100fb9b15952b1a90dce84f
Saubhagya <96274810+saubhagyaranjanmallick@users.noreply.github.com>
2023-09-28 11:38:29 +0530

    video added

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -2,8 +2,10 @@ import React, { useRef } from "react";
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
 import ReactPlayer from "react-player";
+import video from "../../Assets/video.mp4";
+import { Grid } from "@mui/material";
 
-const VIDEO_PATH = "https://www.youtube.com/watch?v=GX_5T6WW9jk";
+// const VIDEO_PATH = "https://www.youtube.com/watch?v=GX_5T6WW9jk";
 const Blog = () => {
   const playerRef = useRef(null);
 
@@ -11,7 +13,9 @@ const Blog = () => {
     <div>
       <Header />
       <div>
-        <ReactPlayer ref={playerRef} url={VIDEO_PATH} controls={true} />
+        <Grid container justifyContent="center">
+          <ReactPlayer  ref={playerRef} url={video} controls={true} />
+        </Grid>
       </div>
       <Footer />
     </div>
